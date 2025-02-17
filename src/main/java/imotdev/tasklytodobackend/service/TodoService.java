4b386973313c75af8e0bb16e7b64169a61a0dc86
Tommy Alhamra <tommy.alhamra@ist.id>
2025-02-17 23:21:56 +0700

    feat: add todo services

---
+++ src/main/java/imotdev/tasklytodobackend/service/TodoService.java
@@ -0,0 +1,36 @@
+package imotdev.tasklytodobackend.service;
+
+import imotdev.tasklytodobackend.model.Todo;
+import imotdev.tasklytodobackend.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+  @Autowired
+  private TodoRepository todoRepository;
+
+  public List<Todo> getAllTodos() {
+    return todoRepository.findAll();
+  }
+
+  public Todo createTodo(Todo todo) {
+    return todoRepository.save(todo);
+  }
+
+  public Todo updateTodo(Long id, Todo updatedTodo) {
+    return todoRepository.findById(id)
+            .map(todo -> {
+              todo.setTitle(updatedTodo.getTitle());
+              todo.setCompleted(updatedTodo.getCompleted());
+              return todoRepository.save(todo);
+            })
+            .orElseThrow(() -> new RuntimeException("Todo not found"));
+  }
+
+  public void deleteTodo(Long id) {
+    todoRepository.deleteById(id);
+  }
+}
