7ee9db300d545352f3e96b098866c11538682d7e
Tommy Alhamra <tommy.alhamra@ist.id>
2025-02-17 16:19:36 +0700

    initial project

---
+++ src/main/java/imotdev/tasklytodobackend/TasklyTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package imotdev.tasklytodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklyTodoBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TasklyTodoBackendApplication.class, args);
+  }
+
+}
